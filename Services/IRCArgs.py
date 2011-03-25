75d6b1a1898f77ca116865e63dea370cc1bf712f
Steven <theepicsnail@gmail.com>
2011-03-25 19:31:30 -0400

    Added 'toMe' to the IRCArgs events

--- Services/IRCArgs.py
+++ Services/IRCArgs.py
@@ -1,5 +1,9 @@
 from Hook import bindFunction
 import re
+#
+#   This should contain handy chunks of code that plugins use
+#   but have to write for each plugin separatly
+#
 class IRCArgs:
     
     nickRE = re.compile("^(?P<nick>.*)!.*$")
@@ -8,5 +12,6 @@ class IRCArgs:
         m=self.nickRE.match(event["prefix"])
         if m!=None:
             event["nick"]=m.groupdict()["nick"]
-        else:
-            event["nick"]=None
+
+        event["toMe"] = event["nickname"]==event["target"] #toMe = true|false depending on if it was sent to you
+
