4ce753efbfb79a302e296a1a06e60d3d13a2137a
David Pflug <david@tpflug.com>
2011-05-03 20:07:54 -0400

    Working Google Define :D

--- Plugins/Google_Define.py
+++ Plugins/Google_Define.py
@@ -4,13 +4,12 @@ from Hook import bindFunction, requires, prefers
 @requires("Google")
 @prefers("Colors")
 class Google_Define:
-    @bindFunction(message="!define")
-    def g_define(self, response, target, colorize, gdefine):
+    @bindFunction(message="!gd (?P<term>\w+) ?(?P<definition>\d*)")
+    def g_define(self, term, response, target, colorize, gdefine, definition):
+        print(definition)
         if colorize:
-            print("<Google Define: %s | {LINK}%s{} [{B}%s{} of %s]>" % gdefine("test")) # debug
             return response.msg(target, colorize(
-                "<{C3}Google Define{}: %s | {LINK}%s{} [{B}%s{} of %s]>" % gdefine("test")))
+                "<{C3}Google Define{}: %s | {LINK}%s{} [{B}%s{} of %s]>" % gdefine(term, definition)))
         else:
-            print("<Google Define: %s | {LINK}%s{} [{B}%s{} of %s]>" % gdefine("test")) # debug
             return response.msg(target,
-                    "<Google Define: %s | {LINK}%s{} [{B}%s{} of %s]>" % gdefine("test"))
+                    "<Google Define: %s | {LINK}%s{} [{B}%s{} of %s]>" % gdefine(term, definition))
--- Services/Google.py
+++ Services/Google.py
@@ -4,10 +4,11 @@ import lxml.html
 from Hook import bindFunction
 
 GOOGLE_SEARCH_URL = "http://www.google.com/search?&"
+HEADERS  = { 'User-Agent': "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.5)" }
 
 
 class Google:
-    def _buildResponse(url, is_json=False):
+    def _buildResponse(self, url, is_json=False):
         request = urllib2.Request(url, None, HEADERS)
         response = urllib2.urlopen(request)
         if is_json:
@@ -17,7 +18,7 @@ class Google:
             return response['responseData']
         return response
 
-    def gdefine(self, term, num=None):
+    def gdefine(self, term, num=0):
         """
         Returns google definition for query.
 
@@ -25,6 +26,10 @@ class Google:
         term -- contains query value.
         num -- Which result to return (default 0)
         """
+        if num == '':
+            num = 0
+        num = int(num)
+
         query = { 'q': 'define:' + term }
         url = GOOGLE_SEARCH_URL + urllib.urlencode(query)
         response = self._buildResponse(url)
