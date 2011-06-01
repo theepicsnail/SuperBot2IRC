e2285ca1d31213425770a5882ca99b2171d13c70
David Pflug <david@tpflug.com>
2011-06-01 15:54:24 -0400

    More robust URLUtils
    Attempts to use lxml, falls back to BeautifulSoup, falls back again to
    string handling.

--- Services/URLUtils.py
+++ Services/URLUtils.py
@@ -1,22 +1,34 @@
 import re
 import urllib2
+# Try lxml, fall back to BeautifulSoup, keep falling to string parsing
 try:
-    from BeautifulSoup import BeautifulStoneSoups
+    import lxml.html
+except ImportError:
+    lxml = False
+
+try:
+    from BeautifulSoup import BeautifulSoup, BeautifulStoneSoups
 except ImportError:
     import htmlentitydefs
     import re
-    BeautifulStoneSoup = False
+    BeautifulStone = False
 
 
 class URLUtils:
     def grabTitle(self, url):
         """Return the title of the page if exists."""
         try:
-            title = urllib2.urlopen(url, None, 5).read(5120).split("title>")[1][:-2]
-            title = re.sub('[\n\r\t ]+', ' ', title).strip()
-            if BeautifulStoneSoup:
+            pagepart = urllib2.urlopen(url, None, 5).read(5120)
+            if lxml:
+                title = lxml.html.fromstring(pagepart).find(".//title").text
+                return re.sub('[\n\r\t ]+', ' ', title).strip()
+            elif BeautifulSoup:
+                title = BeautifulSoup(pagepart)
+                title = re.sub('[\n\r\t ]+', ' ', title).strip()
                 return BeautifulStoneSoup(title, convertEntities=BeautifulStoneSoup.HTML_ENTITIES).contents[0]
             else:
+                title = pagepart.split("title>")[1][:-2]
+                title = re.sub('[\n\r\t ]+', ' ', title).strip()
                 return self.unescape(str(title))
         except Exception, e:
             raise e
@@ -41,7 +53,7 @@ class URLUtils:
                     text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
                 except KeyError:
                     pass
-            return text # leave as is
+            return text  # leave as is
         return re.sub("&#?\w+;", fixup, text)
 
     def onEvent(self, event):
