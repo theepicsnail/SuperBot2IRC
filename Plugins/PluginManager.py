8f28e6e4cad0ed2c8db0e33966914d7638d492e4
UdderWeb Team <staff@udderweb.com>
2012-01-25 18:15:14 -0500

    Fixed bugs in last submission >.>

--- Plugins/PluginManager.py
+++ Plugins/PluginManager.py
@@ -15,20 +15,20 @@ class PluginManager:
         print "load3"
         if plug:
             print "load 4a"
-            pd.EnsureDedicated(pm.getDedicated(),ro,core.handler)
+            pd.EnsureDedicated(pm.GetDedicated(),ro,pd.GetResponseHandler())
             return response.msg(nick,"Plugin %s loaded."%message0)
         else:
             print "load 4b"
             return response.msg(nick,"Plugin %s failed to load."%message0)            
 
     @bindFunction(command="NOTICE", message="^plugin unload (.*)$")
-    def unload(self,pm,response,message0,nick,pm,pd,ro,core):
+    def unload(self,pm,response,message0,nick,pd,ro,core):
         print "unload",nick,message0,pm
         if not pm.HasPlugin(message0):
             return response.msg(nick,"Plugin %s wasn't loaded"%message0)
 
         if pm.UnloadPlugin(message0):
-            pd.EnsureDedicated(pm.getDedicated(),ro,core.handler)
+            pd.EnsureDedicated(pm.GetDedicated(),ro,pd.GetResponseHandler())
             return response.msg(nick,"Plugin %s is nolonger loaded."%message0)
         else:
             return response.msg(nick,"Couldn't unload %s for some reason. Check the log!"%message0)
