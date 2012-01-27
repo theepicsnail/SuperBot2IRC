2fb6f23682328a0d3ca32a1c57e534960c352717
UdderWeb Team <staff@udderweb.com>
2012-01-27 11:07:18 -0500

    Added persistant storage

--- Plugins/Alarm.py
+++ Plugins/Alarm.py
@@ -4,6 +4,9 @@ from Logging import LogFile
 log = LogFile("Alarm")
 import time
 import datetime
+
+
+storeFile = "AlarmStore.pkl"
 parseStrings = [
 "%m/%d",
 "%m/%d/%y",
@@ -29,23 +32,39 @@ except:
     parser = backupParse
     log.debug("Using backup parser")
 
+try:
+    import cPickle as pickle
+    log.debug("Using cPickle")
+except:
+    import pickle
+    log.debug("Using pickle.")
+
+
+
+
+
 @requires("IRCArgs")
 class Alarm:
     alarms = []
+    def __init__(self):
+        try:
+            self.alarms = pickle.load(file(storeFile))
+            log.debug("Pickle loaded.",*self.alarms)
+        except:
+            log.exception("Failed to load pickle.")
+            self.alarms = []
+            pass
+
     @dedicated()
     def ded(self,response):
         time.sleep(1)
-        if len(self.alarms)==0:
-            return
-        log.debug("Alarms exist. Checking time.")
-        now = datetime.datetime.now()
-        curAlarm = self.alarms[0]
-        log.debug("Next alarm:",curAlarm)
-        log.debug(curAlarm[0] > now)
-        while curAlarm[0] < now:
-            yield response.msg(curAlarm[1],curAlarm[2])
-            self.alarms.pop(0)
+        while len(self.alarms)>0:
+            now = datetime.datetime.now()
             curAlarm = self.alarms[0]
+            if now > curAlarm[0]:
+                log.debug("Firing alarm:",*curAlarm) 
+                yield response.msg(curAlarm[1],curAlarm[2])
+                self.alarms.pop(0)
 
 
     @bindFunction(message="!alarm (?P<when>.*):(?P<what>.*)")
@@ -54,11 +73,16 @@ class Alarm:
         dt = parser(when)
         if not dt:
             log.warning("Invalid time.",when)
-            return response.msg(target,"Sorry, I couldn't figure out when \"%s\" is."%when)
+            yield response.msg(target,"Sorry, I couldn't figure out when \"%s\" is."%when)
+            return
         log.debug("Scheduled alarm:",when,target,what)
+        yield response.msg(target,"Sceduled for: %s"%dt)
         self.alarms.append((dt,target,what))
         self.alarms.sort()
-
-
+        
+        try:
+            pickle.dump(self.alarms,file(storeFile,"w"))
+        except:
+            log.exception("Failed to store pickle.")
 
 
