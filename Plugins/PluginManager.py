0b39be4fdd453fb1f61a5455986ae27b99071a67
UdderWeb Team <staff@udderweb.com>
2012-01-27 11:06:43 -0500

    Fixed PluginManager

--- Plugins/PluginManager.py
+++ Plugins/PluginManager.py
@@ -1,39 +1,47 @@
 from Hook import bindFunction,requires
-
-#pd.EnsureDedicated(pm.GetDedicated(),self._ResponseObject,handler)
+from Logging import LogFile
+log = LogFile("PluginManager2")
 
 @requires("IRCArgs")
 @requires("PluginManager")
 class PluginManager:
     @bindFunction(command="NOTICE", message="^plugin load (.*)$")
     def load(self,response,message0,nick,pm,pd,ro,core):
-        print "load",nick,message0,pm
+        log.debug("Loading plugin",message0)
         if pm.HasPlugin(message0):
+            log.warning("Plugin already loaded.")
             return response.msg(nick,"Plugin is already loaded. Use cycle to force a reload.")
-        print "load2"
+
         plug = pm.LoadPlugin(message0)
-        print "load3"
         if plug:
-            print "load 4a"
+            log.debug("Plugin loaded. Updating dedicated threads.")
             pd.EnsureDedicated(pm.GetDedicated(),ro,pd.GetResponseHandler())
+            log.debug("Finished loading plugin.")
             return response.msg(nick,"Plugin %s loaded."%message0)
         else:
-            print "load 4b"
+            log.warning("Plugin failed to load. Check PluginManager's logs.")
             return response.msg(nick,"Plugin %s failed to load."%message0)            
 
     @bindFunction(command="NOTICE", message="^plugin unload (.*)$")
     def unload(self,pm,response,message0,nick,pd,ro,core):
-        print "unload",nick,message0,pm
+        log.debug("Unloading plugin",message0)
         if not pm.HasPlugin(message0):
+            log.warning("Plugin was not loaded.")
             return response.msg(nick,"Plugin %s wasn't loaded"%message0)
 
         if pm.UnloadPlugin(message0):
+            log.debug("Unloaded plugin. Updating dedicated threads.")
             pd.EnsureDedicated(pm.GetDedicated(),ro,pd.GetResponseHandler())
+            log.debug("Finished unloading plugin.")
             return response.msg(nick,"Plugin %s is nolonger loaded."%message0)
         else:
+            log.warning("Unload failed. Check PluginManager's logs.")
             return response.msg(nick,"Couldn't unload %s for some reason. Check the log!"%message0)
 
     @bindFunction(command="NOTICE", message="^plugin cycle$")
     @bindFunction(command="NOTICE", message="^plugin cycle (.*)$")
-    def cycle(self,response,message0="ALL"):
-        print "cycle",message0
+    def cycle(self,pm,response,nick,pd,ro,core,message0="ALL"):
+        log.debug("Starting cycle of plugin",message0)
+        self.unload(pm,response,message0,nick,pd,ro,core)
+        self.load(response,message0,nick,pm,pd,ro,core)
+        log.debug("Cycle finished.")
