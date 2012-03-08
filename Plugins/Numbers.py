dae830712303a4aae7e9366c1d6136b1c529a4f0
David Pflug <david@tpflug.com>
2012-03-08 13:44:35 -0500

    Faster factor for Numbers

--- Plugins/Numbers.py
+++ Plugins/Numbers.py
@@ -2,6 +2,7 @@ import time
 import random
 import re
 import urllib
+from c_math import fact
 from Hook import *
 from Logging import LogFile
 log = LogFile("Numbers")
@@ -24,16 +25,8 @@ def chooseNumbers(numLarge):
 
 
 def factor(n):
-    if n == 1: return [1]
-    i = 2
-    limit = n**0.5
-    while i <= limit:
-        if n % i == 0:
-            ret = factor(n/i)
-            ret.append(i)
-            return ret
-        i += 1
-    return [n]
+    return fact(n)
+
 @requires("IRCArgs")
 class Numbers:
     numbers = None#([],0)Current numbers and goal
